e2bec336b31c2306b4fdcdd7ab6bdb2ca894a1bf
Oana-Florentina <floryflorentina55@gmail.com>
2023-06-16 00:19:02 +0300

    integrated product service

--- server/index.php
+++ server/index.php
@@ -17,14 +17,14 @@ $method = $_SERVER['REQUEST_METHOD'];
 $url = $_SERVER['REQUEST_URI'];
 */
 
-
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
      if ($_SERVER['REQUEST_URI'] === '/api/products') {
+
+        // $contoller = new ProductsController($dbConnection, )
 
          $products = [
              ['id' => 1, 'name' => 'Product 1', 'price' => 10.99],
