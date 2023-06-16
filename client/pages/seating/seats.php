3326e1c110daefc27c02fc72d7439aa451215b27
Oana-Florentina <floryflorentina55@gmail.com>
2023-06-17 02:07:31 +0300

    ceva modificari pe front, verdele ala era oribil..:)

--- client/pages/seating/seats.php
+++ client/pages/seating/seats.php
@@ -13,14 +13,14 @@
                             <svg>
                                 <!-- table -->
                                 <a href="./pay.php">
-                                    <circle id="round-table4" r="45" cx="90" cy="90" />
+                                    <circle id="round-table" r="45" cx="90" cy="90" />
                                     <text x="45%" y="53%" text-anchor="middle" font-size="20" fill="white">Table 1</text>
                                 </a>
                                 <!-- seats -->
-                                <circle id="seat0" cx="45" cy="45" r="15" />
-                                <circle id="seat1" cx="135" cy="45" r="15" fill="#273E2B" />
-                                <circle id="seat2" cx="45" cy="135" r="15" fill="#273E2B" />
-                                <circle id="seat3" cx="135" cy="135" r="15" fill="#273E2B" />
+                                <circle id="seat" cx="45" cy="45" r="15" />
+                                <circle id="seat" cx="135" cy="45" r="15"  />
+                                <circle id="seat" cx="45" cy="135" r="15"  />
+                                <circle id="seat" cx="135" cy="135" r="15" />
 
                             </svg>
                         </div>
