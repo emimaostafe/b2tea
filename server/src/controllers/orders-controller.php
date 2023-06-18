8a983a874cab33dbefb7fa26392ed48911d705b6
Emima <emima.ostafe@gmail.com>
2023-06-18 21:54:30 +0300

    last GOOD version

---
+++ server/src/controllers/orders-controller.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'controllers/controller.php';
+require_once 'services/products-service.php';
+
+class ProductsController implements ControllerInterface
+{
+    private $productsService;
+    private $uri;
+    private $method;
+    private $payload;
+
+    public function __construct($uri, $method, $payload = null)
+    {
+        $this->productsService = new ProductsService();
+        $this->uri = $uri;
+        $this->method = $method;
+        $this->payload = $payload;
+
+    }
+
+    public function handle(): string
+    {
+        switch ($this->method) {
+            case "GET":
+                return $this->handleGet();
+            default:
+                return "Unresolved endpoint";
+        }
+    }
+
+    private function handleGet(): string
+    {
+        if ($this->isGetById()) {
+            $id = $this->parts()[3];
+
+            return $this->productsService->getById($id);
+        }
+
+        if ($this->isGetFavorites()) {
+            return $this->productsService->getFavorites();
+        }
+
+        return $this->productsService->getAll();
+    }
+
+    private function parts()
+    {
+        return explode("/", $this->uri);
+    }
+
+    private function isGetById()
+    {
+        return count($this->parts()) === 4 && ($this->parts()[3]) > 0;
+    }
+
+    private function isGetFavorites()
+    {
+        return count($this->parts()) === 4 && end($this->parts()) === 'favorites';
+    }
+}
--- server/src/services/products-service.php
+++ server/src/services/products-service.php
@@ -9,18 +9,9 @@ class ProductQueries
     
     public static $getById = "SELECT * FROM ProductsMenu WHERE id = ?"; 
 
-<<<<<<< HEAD
-    public static $getFavorites = "SELECT * FROM ProductsORDER BY rating DESC LIMIT 4";
-
-=======
-<<<<<<< HEAD
     public static $getFavorites = "SELECT * FROM products ORDER BY rating DESC LIMIT 4";
 
 
-=======
-    public static $getFavorites = "SELECT * FROM Products ORDER BY rating DESC LIMIT 4";
->>>>>>> 849dd0c61a7c4f68825afb83891f3d8b6659f5c8
->>>>>>> 539b298e44f71af4be10a1186eac50a2ab5af83e
 }
 
 class ProductsService
@@ -51,35 +42,6 @@ class ProductsService
         $result = $this->database->getById(ProductQueries::$getById, $id);
         return $this->mapToArray($result);
     }
-
-<<<<<<< HEAD
-  public function getFavorites(): string
-  {
-      $result = $this->database->query(ProductQueries::$getFavorites);
-      $products = $this->mapToArray($result);
-      $jsonResponse = json_encode($products);
-      return substr($jsonResponse, strpos($jsonResponse, "["));
-  }
-
-
-  private function mapToArray($result): array
-  {
-      $products = array();
-      while ($row = $result->fetch_assoc()) {
-          $products[] = array(
-              'id' => $row['id'],
-              'name' => $row['name'],
-              'description' => $row['description'],
-              'imageUrl' => $row['imageUrl'],
-              'rating' => $row['rating']
-          );
-      }
-
-      return $products;
-  }
-
-
-=======
     private function mapToArrayFavorites($result): string
     {
         $products = array();
@@ -99,5 +61,4 @@ class ProductsService
 
         return json_encode($products);
     }
->>>>>>> 849dd0c61a7c4f68825afb83891f3d8b6659f5c8
 }
